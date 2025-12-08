3d6e42ec57369eb7f8d818571a35d0ecafc7fb80
Pablo <pablo.silva.edu@gmail.com>
2025-12-08 00:41:06 -0300

    Adicionadas as imagens de 'frases' na seção de figurinhas

--- Barber/src/pages/settings/StickersGallery.tsx
+++ Barber/src/pages/settings/StickersGallery.tsx
@@ -32,7 +32,11 @@ const getStickerFiles = (folderName: string): string[] => {
       '121.png', '122.png', '123.png', '124.png', '125.png', '126.png', '127.png', '128.png', '129.png', '130.png',
       '131.png', '132.png', '133.png', '134.png', '135.png'
     ],
-    frases: [],
+    frases: [
+      '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png',
+      '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png', '18.png', '19.png', '20.png',
+      '21.png', '22.png', '23.png', '24.png', '25.png', '26.png', '27.png', '28.png', '29.png', '30.png'
+    ],
     favela: [
       '1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png',
       '11.png', '12.png', '13.png', '14.png', '15.png', '16.png', '17.png'
